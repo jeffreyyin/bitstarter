9bf1f289a3fb9c2e7712277e5f92474b3368fb8e
JeffreyYin <jy1999us@yahoo.com>
2013-07-10 19:03:46 -0400

    update web.js

--- web.js
+++ web.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World updated!');
+  //response.send('Hello World updated!');
+	fs.readFile('index.html', function(error, content){
+		if (error) {
+			console.log(error);
+		}
+		else {
+			response.write(content);
+		}
+	}
 });
 
 var port = process.env.PORT || 5000;
